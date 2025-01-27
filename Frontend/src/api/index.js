3ab22d95a901114b72bfdd45d51e1f7fe14063f1
KanchanKR <renapurkarkanchan@gmail.com>
2025-01-27 22:49:55 +0530

    updated the code

--- Frontend/src/api/index.js
+++ Frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "https://full-stack-task-management-app-f4pa.onrender.com/api/",
 });
 
 //auth
